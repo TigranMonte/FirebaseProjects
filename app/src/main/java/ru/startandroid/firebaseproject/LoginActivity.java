39844f355b7dac0abdc337f6bee01aefa0d476ad
Tigran <tigranma@gmail.com>
2021-11-30 17:47:54 +0300

    Firebase Authentication and Verification Email

--- app/src/main/java/ru/startandroid/firebaseproject/LoginActivity.java
+++ app/src/main/java/ru/startandroid/firebaseproject/LoginActivity.java
@@ -1,50 +1,163 @@
 package ru.startandroid.firebaseproject;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
-
     private EditText edLogin, edPassword;
-    private FirebaseAuth myAuth;
+    private FirebaseAuth mAuth;
+    private Button btnStart, btnSignUp, btnSignIn, btnSignOut;
+    private TextView tvUserName;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        init();
 
-        edLogin = findViewById(R.id.edLogin);
-        edPassword = findViewById(R.id.edPassword);
-        myAuth = FirebaseAuth.getInstance();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        FirebaseUser currentUser = myAuth.getCurrentUser();
-        if (currentUser != null) {
-            Toast.makeText(this, "User already exists", Toast.LENGTH_SHORT).show();
+        FirebaseUser cUser = mAuth.getCurrentUser();
+        if(cUser != null)
+        {
+            showSigned();
+            String userName = "Вы вошли как : " + cUser.getEmail();
+            tvUserName.setText(userName);
+
+            Toast.makeText(this, "User not null", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            notSigned();
+            Toast.makeText(this, "User null", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void init()
+    {
+        edLogin = findViewById(R.id.edLogin);
+        edPassword = findViewById(R.id.edPassword);
+        mAuth = FirebaseAuth.getInstance();
+        tvUserName = findViewById(R.id.tvUserEmail);
+        btnStart = findViewById(R.id.btnStart);
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        btnSignOut = findViewById(R.id.btnSignOut);
+    }
+    public void onClickSignUp(View view)
+    {
+        if(!TextUtils.isEmpty(edLogin.getText().toString()) && !TextUtils.isEmpty(edPassword.getText().toString()))
+        {
+            mAuth.createUserWithEmailAndPassword(edLogin.getText().toString(),edPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                    {
+                        showSigned();
+                        sendEmailVer();
+
+                        Toast.makeText(getApplicationContext(), "User SignUp Successful", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        notSigned();
+                        Toast.makeText(getApplicationContext(), "User SignUp failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), "Please entre Email and Password", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void onClickSignIn(View view)
+    {
+        if(!TextUtils.isEmpty(edLogin.getText().toString()) && !TextUtils.isEmpty(edPassword.getText().toString())){
+            mAuth.signInWithEmailAndPassword(edLogin.getText().toString(),edPassword.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                    {
+                        showSigned();
+                        Toast.makeText(getApplicationContext(), "User SignIn Successful", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        notSigned();
+                        Toast.makeText(getApplicationContext(), "User SignIn failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+    public void onClickSignOut(View view){
+        notSigned();
+    }
+
+    private void showSigned(){
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user.isEmailVerified()) {
+            String userName = "Вы вошли как : " + user.getEmail();
+            tvUserName.setText(userName);
+            btnStart.setVisibility(View.VISIBLE);
+            tvUserName.setVisibility(View.VISIBLE);
+            btnSignOut.setVisibility(View.VISIBLE);
+            edLogin.setVisibility(View.GONE);
+            edPassword.setVisibility(View.GONE);
+            btnSignUp.setVisibility(View.GONE);
+            btnSignIn.setVisibility(View.GONE);
         } else {
-            Toast.makeText(this, "New user", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "Check your email", Toast.LENGTH_SHORT).show();
         }
     }
 
-    public void onClickSignUp(View view){
-        if (!TextUtils.isEmpty(edLogin.getText().toString()) &&
-                !TextUtils.isEmpty(edPassword.getText().toString())) {
-            myAuth.createUserWithEmailAndPassword();
-        }
+    private void notSigned() {
+        btnStart.setVisibility(View.GONE);
+        tvUserName.setVisibility(View.GONE);
+        btnSignOut.setVisibility(View.GONE);
+        edLogin.setVisibility(View.VISIBLE);
+        edPassword.setVisibility(View.VISIBLE);
+        btnSignUp.setVisibility(View.VISIBLE);
+        btnSignIn.setVisibility(View.VISIBLE);
     }
 
-    public void onClickSignIn(View view){
+    public void onClickStart(View view) {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
 
+    private void sendEmailVer() {
+        FirebaseUser user = mAuth.getCurrentUser();
+        assert  user != null;
+        user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Check your email", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Send email failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
